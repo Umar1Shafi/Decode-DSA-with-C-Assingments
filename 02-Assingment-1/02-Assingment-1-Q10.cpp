9dfdb4e133a110f751abbbfd64e9f713a91cd432
Umar Shafi <101823809+Umar1Shafi@users.noreply.github.com>
2024-05-04 19:14:08 +0530

    Update 02-Assingment-1-Q10.cpp

--- 02-Assingment-1/02-Assingment-1-Q10.cpp
+++ 02-Assingment-1/02-Assingment-1-Q10.cpp
@@ -1,8 +1,2 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int i = (4 + 7 / 5 * 6 * 6 + 9) % 100;
-    cout << "The output of the code is: " << i << endl;
-    return 0;
-}
+Output
+value of b and c are respectively 300 and 200
